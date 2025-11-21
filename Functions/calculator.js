a766aebdd431fd0343417e81b30695b9f4c2b853
Julczan <julek111222h@gmail.com>
2025-11-21 12:10:42 +0100

    test functions of calculator object
    Testing funciton for adding, subtracting,
    multiplying and dividing.

--- Functions/calculator.js
+++ Functions/calculator.js
@@ -3,21 +3,26 @@ const calculator = {
     if (typeof num1 !== "number" || typeof num2 !== "number") {
       return "Please input two numbers";
     }
+
+    return num1 + num2;
   },
   subtract: function (num1, num2) {
     if (typeof num1 !== "number" || typeof num2 !== "number") {
       return "Please input two numbers";
     }
+    return num1 - num2;
   },
   divide: function (num1, num2) {
     if (typeof num1 !== "number" || typeof num2 !== "number") {
       return "Please input two numbers";
     }
+    return num1 / num2;
   },
   multiply: function (num1, num2) {
     if (typeof num1 !== "number" || typeof num2 !== "number") {
       return "Please input two numbers";
     }
+    return num1 * num2;
   },
 };
 
--- Tests/calculator.test.js
+++ Tests/calculator.test.js
@@ -1,14 +1,32 @@
 import calculator from "../Functions/calculator";
 
 test("Input provides two numbers", () => {
+  expect(calculator.divide("string")).toBe("Please input two numbers");
+  expect(calculator.multiply("string")).toBe("Please input two numbers");
+  expect(calculator.subtract("string")).toBe("Please input two numbers");
   expect(calculator.add("string")).toBe("Please input two numbers");
 });
-test("Input provides two numbers", () => {
-  expect(calculator.subtract("string")).toBe("Please input two numbers");
+
+test("add function returns the result", () => {
+  expect(calculator.add(2, 2)).toBe(4);
+  expect(calculator.add(-2, 2)).toBe(0);
+  expect(calculator.add(100, 21)).toBe(121);
 });
-test("Input provides two numbers", () => {
-  expect(calculator.divide("string")).toBe("Please input two numbers");
+
+test("subtract function returns the result", () => {
+  expect(calculator.subtract(2, 2)).toBe(0);
+  expect(calculator.subtract(2, 10)).toBe(-8);
+  expect(calculator.subtract(200, 10)).toBe(190);
 });
-test("Input provides two numbers", () => {
-  expect(calculator.multiply("string")).toBe("Please input two numbers");
+
+test("divide function returns the result", () => {
+  expect(calculator.divide(2, 2)).toBe(1);
+  expect(calculator.divide(8, 4)).toBe(2);
+  expect(calculator.divide(10, -3)).toBeCloseTo(-3.33);
+});
+
+test("multiply function returns the result", () => {
+  expect(calculator.multiply(2, 2)).toBe(4);
+  expect(calculator.multiply(12, 2)).toBe(24);
+  expect(calculator.multiply(-2, 50)).toBe(-100);
 });
