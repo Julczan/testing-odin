6f8bb330754945414212d030825be3efe02a8041
Julczan <julek111222h@gmail.com>
2025-11-23 20:48:19 +0100

    get the average of numbers

--- Functions/analyzeArray.js
+++ Functions/analyzeArray.js
@@ -2,6 +2,28 @@ function analyzeArray(array) {
   if (!array) {
     return "Please provide an array of numbers";
   }
+
+  const average = getAverage(array);
+
+  return { average };
+}
+
+function getAverage(array) {
+  const numbers = [];
+
+  for (let i = 0; i < array.length; i++) {
+    numbers.push(array[i]);
+  }
+
+  let sum = 0;
+
+  numbers.forEach((number) => {
+    sum += number;
+  });
+
+  const average = sum / numbers.length;
+
+  return average;
 }
 
 export default analyzeArray;
--- Tests/analyzeArray.test.js
+++ Tests/analyzeArray.test.js
@@ -3,3 +3,9 @@ import analyzeArray from "../Functions/analyzeArray";
 test("Input is an array of numbers", () => {
   expect(analyzeArray()).toBe("Please provide an array of numbers");
 });
+
+test("Function returns the number's average", () => {
+  expect(analyzeArray([2, 2])).toEqual({ average: 2 });
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({ average: 4 });
+  expect(analyzeArray([1, 8])).toEqual({ average: 4.5 });
+});
